1546d7fe50aa2853e40d2cea91066b9e3506edd0
maganathan212003 <meganathan212003@gmail.com>
2026-07-30 23:27:46 +0530

    solve new sum

---
+++ src/Arrays/ValidAnagram.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent")); // true
+        System.out.println(isAnagram("rat", "car"));        // false
+    }
+}

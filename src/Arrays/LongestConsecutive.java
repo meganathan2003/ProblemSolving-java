ba4fe68d981421249a147bf6bd5c2e71a20e43da
maganathan212003 <meganathan212003@gmail.com>
2026-08-08 23:35:36 +0530

    add new sum

---
+++ src/Arrays/LongestConsecutive.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+public class LongestConsecutive {
+    public static int longestConsecutive(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int maxLength = 0;
+
+        for (int num : set) {
+
+            // Start of a sequence
+            if (!set.contains(num - 1)) {
+
+                int current = num;
+                int length = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    length++;
+                }
+
+                maxLength = Math.max(maxLength, length);
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {100, 4, 200, 1, 3, 2};
+
+        System.out.println(longestConsecutive(nums));
+    }
+}

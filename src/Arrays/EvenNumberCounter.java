2209980842ddbe15881d9345dac158331f87ff7e
maganathan212003 <meganathan212003@gmail.com>
2026-08-17 23:48:28 +0530

    add new sum

---
+++ src/Arrays/EvenNumberCounter.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class EvenNumberCounter {
+
+    static int countEvenNumbers(int[] arr) {
+        int count = 0;
+
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 7, 8, 10, 13};
+
+        int result = countEvenNumbers(arr);
+
+        System.out.println(result);
+    }
+}

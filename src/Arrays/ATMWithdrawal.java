5d9719926e0d281e7d3da24cd57742bcedcf87ec
maganathan212003 <meganathan212003@gmail.com>
2026-08-24 23:57:18 +0530

    add new sum

---
+++ src/Arrays/ATMWithdrawal.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.*;
+
+class ATMWithdrawal {
+
+    static int minimumNotes(int[] notes, int amount) {
+
+        int count = 0;
+
+        for (int note : notes) {
+
+            count += amount / note;
+            amount = amount % note;
+        }
+
+        return count;
+    }
+
+    static void main(String[] args) {
+
+        int[] notes = {2000, 500, 200, 100};
+        int amount = 4700;
+
+        System.out.println(minimumNotes(notes, amount));
+    }
+}

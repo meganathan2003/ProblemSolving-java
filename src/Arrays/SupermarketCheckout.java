221d48082dddab499b6a7571ab635c5887fe4aaf
maganathan212003 <meganathan212003@gmail.com>
2026-08-28 23:46:50 +0530

    add new problem

---
+++ src/Arrays/SupermarketCheckout.java
@@ -0,0 +1,58 @@
+package Arrays;
+
+import java.util.*;
+
+class Customer {
+
+    int items;
+
+    Customer(int items) {
+        this.items = items;
+    }
+}
+
+public class SupermarketCheckout {
+
+    public static int totalTime(Customer[] customers, int k) {
+
+        Queue<Customer> queue = new LinkedList<>();
+
+        for (Customer customer : customers) {
+            queue.add(customer);
+        }
+
+        int time = 0;
+
+        while (!queue.isEmpty()) {
+
+            Customer customer = queue.poll();
+
+            int processed = Math.min(customer.items, k);
+
+            time += processed;
+            customer.items -= processed;
+
+            if (customer.items > 0) {
+                queue.add(customer);
+            }
+        }
+
+        return time;
+    }
+
+     static void main(String[] args) {
+
+        Customer[] customers = {
+                new Customer(5),
+                new Customer(2),
+                new Customer(8),
+                new Customer(3)
+        };
+
+        int k = 3;
+
+        int result = totalTime(customers, k);
+
+        System.out.println("Total time = " + result + " seconds");
+    }
+}
